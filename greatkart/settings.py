6b3ec84293ed8c6ce100241ab6408996d9e4f5b3
Dhruv Takwal <dhruv@rapidinnovation.dev>
2021-12-01 17:19:56 +0530

    Update heroku setup

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-scfa#s_i3a(zm8r^rfrsky6#3iudl4+2ebj0w*p-+()14jvy0h'
+# SECRET_KEY = 'django-insecure-scfa#s_i3a(zm8r^rfrsky6#3iudl4+2ebj0w*p-+()14jvy0h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
